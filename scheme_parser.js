f6470c11f55ca09239cf51a26fc95d97364f5fea
swapnil <swapnilpatil427@gmail.com>
2016-11-28 00:23:19 -0800

    Rejected commit: Default

---
+++ scheme_parser.js
@@ -0,0 +1,58 @@
+var peg = require("pegjs");
+
+var parser = peg.generate(
+    ' start = expression;\
+  validchar = [a-zA-Z_?!+\\-=@#$%^&*/.];\
+  spaces = \" \"*;\
+  newline = [\\n]*;\
+  digit = [0-9];\
+  atom =    spaces newline chars:validchar+ spaces newline   { return chars.join(\"\"); }\
+          / spaces newline numbers:digit+ spaces newline     { return parseInt(numbers.join(\"\")); };\
+  list =    spaces newline \"(\" spaces newline expressions:expression+ newline spaces\")\" { return expressions; };\
+  expression = atom / list');
+
+var interprete = function (expr) {
+    //console.log(expr[0]);
+    if (typeof expr === 'number') {
+        console.log("number");
+        return expr;
+    }
+    // Strings are variable references
+    if (typeof expr === 'string') {
+        console.log(expr);
+        //return sigma[expr];
+        return expr;
+    }
+
+    switch (expr[0]) {
+        case '+':
+            return interprete(expr[1]) +
+                interprete(expr[2]);
+        case '-':
+            return interprete(expr[1]) -
+                interprete(expr[2]);
+        case '*':
+            return interprete(expr[1]) *
+                interprete(expr[2]);
+        case '/':
+            return interprete(expr[1]) /
+                interprete(expr[2]);
+        case 'alert' :
+            return "alert(" + interprete(expr[1]) + ")"
+
+    }
+};
+
+var example = "(alert( + 4 ( + 2 3)))";
+//var example = "( + 4 ( + 2 3))";
+
+
+var AST = parser.parse(example);
+console.log("program : " + example);
+console.log("AST");
+console.log(parser.parse(example));
+
+res = interprete(AST);
+console.log("\nEvaluating Scheme Program: " + res);
+
+
